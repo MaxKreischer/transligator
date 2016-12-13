ab320d66c034b803a1a2bdb15dc283aa3edbb5e6
Max Kreischer <MKreischer@gmx.net>
2016-12-13 03:04:44 +0100

    created .ui gui skeleton

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-#! python3
 """
-Multi line comment legibility test!
-Taken from: A simple addon from Anki addon doc site
+Select .txt file from file browser and create flash-cards from terminology list
+contained therein
 """
-
+import sys
 #import main window obj from aqt
 from aqt import mw
 #import "show info" tool from utils.py
-from aqt.utils import showInfo
+from aqt.utils import getFile
 #import Qt Gui library
 from aqt.qt import *
 
-#function to execute when menu item is selected
 
+#function to execute when menu item is selected
 def importTerminologyFile():
-    cardCount = mw.col.cardCount
-    showInfo("Card count: %d" % cardCount)
+    getFile(mw, "")
+
 
 #create new menu item "Import Terminology"
 action = QAction("Import Terminology", mw)
-#set to call function when clicked
+# Connect signal "triggered" to slot "importTerminologyFile"
 action.triggered.connect(importTerminologyFile)
 #add to tools menu
 mw.form.menuTools.addAction(action)
