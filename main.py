6f941ffc50896b8f7674e64c5f625b96048ecb9d
Max Kreischer <MKreischer@gmx.net>
2016-12-13 03:01:28 +0100

    created .ui gui skeleton

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+#! python3
 """
 Multi line comment legibility test!
-
+Taken from: A simple addon from Anki addon doc site
 """
-import numpy as np
 
-a = np.arange(0,10,.01)
-print(np.size(a))
+#import main window obj from aqt
+from aqt import mw
+#import "show info" tool from utils.py
+from aqt.utils import showInfo
+#import Qt Gui library
+from aqt.qt import *
+
+#function to execute when menu item is selected
+
+def importTerminologyFile():
+    cardCount = mw.col.cardCount
+    showInfo("Card count: %d" % cardCount)
+
+#create new menu item "Import Terminology"
+action = QAction("Import Terminology", mw)
+#set to call function when clicked
+action.triggered.connect(importTerminologyFile)
+#add to tools menu
+mw.form.menuTools.addAction(action)
